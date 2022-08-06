2af57c0eaf4a2ecced32b0e863dcc65d3822dbc9
maycie-morris <maycie.morris@gmail.com>
2022-08-05 21:40:42 -0400

    Navbar, footer created, wallet connect implemented

---
+++ pages/_app.tsx
@@ -0,0 +1,69 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  wallet,
+} from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    chain.mainnet,
+    chain.polygon,
+    chain.optimism,
+    chain.arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
+      : []),
+  ],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_KEY }), publicProvider()]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: 'RainbowKit demo',
+  chains,
+});
+
+const demoAppInfo = {
+  appName: 'Rainbowkit Demo',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [
+      wallet.argent({ chains }),
+      wallet.trust({ chains }),
+      wallet.ledger({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
---
+++ pages/home.tsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+  return (
+    <div id='home_container' className='container'>
+      <div id='header'>
+        <h1>YGG is a play-to-earn gaming guild</h1>
+      </div>
+      <div id='home_contents'>
+        <span>
+          YGG is a play-to-earn gaming guild, bringing players together to earn
+          via blockchain-based economies.
+        </span>
+        <span>We are the settlers of new worlds in the metaverse</span>
+      </div>
+    </div>
+  );
+};
